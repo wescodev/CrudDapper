5c9029b9a7c8bfd79646f197b1e2e521afeeb2ac
wescodev <wesleysouza700@gmail.com>
2025-04-12 00:23:03 -0300

    Criando relacao com mapper para conversao de dados

--- CrudDapper/Services/UsuarioService.cs
+++ CrudDapper/Services/UsuarioService.cs
@@ -1,6 +1,45 @@
-﻿namespace CrudDapper.Services
+﻿using AutoMapper;
+using CrudDapper.DTO;
+using CrudDapper.models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace CrudDapper.Services
 {
-    public class UsuarioService
+    public class UsuarioService : IUsuarioInterface
     {
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+        public UsuarioService(IConfiguration configuration, IMapper mapper)
+        {
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+        public async Task<ResponseModel<List<UsuarioListarDTO>>> BuscarUsuarios()
+        {
+            ResponseModel<List<UsuarioListarDTO>> response = new ResponseModel<List<UsuarioListarDTO>>();
+
+            using (var connectioon = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                var usuariosBanco = await connectioon.QueryAsync<Usuario>("SELECT * FROM USUARIOS");
+
+                if(usuariosBanco.Count() == 0)
+                {
+                    response.Mensagem = "Nenhum usuario localizado!";
+                    response.Status = false;
+                    return response;
+                }
+
+                //Transformacao Mapper
+                var usuarioMapeado = _mapper.Map<List<UsuarioListarDTO>>(usuariosBanco);
+
+                response.Dados = usuarioMapeado;
+                response.Mensagem = "Usuarios localizados com sucesso!";
+
+            }
+
+            return response;
+        }
     }
 }
